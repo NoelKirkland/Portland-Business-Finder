16ce9db1686ae2025e93950e15b209a124286248
Noel Kirkland <noelkirkland@gmail.com>
2020-11-15 14:52:06 -0800

    create search bar component

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation"
+import { createStackNavigator } from "react-navigation-stack"
+import SearchSreen from './src/screens/SearchScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+  Search: SearchSreen,
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Buisness Search'
+  }
 });
+
+export default createAppContainer(navigator)
---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons'
+
+const SearchBar = ({ term, onTermChange, onTermSubmit}) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name='search'style={styles.iconStyle} />
+      <TextInput
+        autoCapitalize='none'
+        autoCorrect={false}
+        placeholder='search'
+        style={styles.inputStyle}
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row',
+    marginTop: 10
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15
+  }
+});
+
+export default SearchBar;
