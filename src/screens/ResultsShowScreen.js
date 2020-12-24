5cf8879078fb3d8adf5a360772f3462a33b7daea
Noel Kirkland <noelkirkland@gmail.com>
2020-12-24 12:58:26 -0800

    get buisness details for display in ResultsShowScreen

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import yelp from '../api/yelp';
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
+  const id = navigation.getParam('id');
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
+
   return (
     <View>
-      <Text>Results Show Screen</Text>
+      <Text>{result.name}</Text>
+      <Text>{result.location.display_address}</Text>
+      <FlatList 
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item}} />
+        }}
+      />
+      <Text>{result.hours[0].is_open_now ? null : 'Currently closed'}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300
+  }
+});
 
 export default ResultsShowScreen;
