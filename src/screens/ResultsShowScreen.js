5d52a84bf1814c436b23e915638c829920520f3a
Noel Kirkland <noelkirkland@gmail.com>
2020-12-24 00:18:46 -0800

    create ResultsShowScreen component

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ResultsShowScreen = () => {
+  return (
+    <View>
+      <Text>Results Show Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;
