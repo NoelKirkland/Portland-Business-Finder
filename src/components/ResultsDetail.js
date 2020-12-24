59609dc2519684297d3338ced9a08aa1a9ae8e13
Noel Kirkland <noelkirkland@gmail.com>
2020-12-23 23:37:02 -0800

    pull buisness image, name, rating, and number of reviews into my ResultsDetail component for display

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
-const ResultsDetail = () => {
-  return <View>
-    <Text>Results detail</Text>
-  </View>
+const ResultsDetail = ({ result }) => {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{uri: result.image_url}} />
+      <Text style={styles.name}>{result.name}</Text>
+      <Text>{result.rating} Stars, {result.review_count}</Text>
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 10,
+  },
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4
+  },
+  name: {
+    fontWeight: 'bold',
+    
+  }
+});
 
 export default ResultsDetail;
